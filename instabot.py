8c194451762d8bfb0d4cc8cf13b1db4e7c3bb0aa
mikemds <mikemds@udel.edu>
2020-10-06 12:46:29 -0400

    added tags, follow, comments, and max quota methods

--- instabot.py
+++ instabot.py
@@ -2,3 +2,12 @@ from instapy import InstaPy
 
 session = InstaPy(username="__instapy__", password="UQBu92kXveT8HgK")
 session.login()
+session.like_by_tags(["cyberpunk2077"])
+session.set_dont_like(["naked", "nsfw"])
+session.set_do_follow(True, percentage=50)
+session.set_do_comment(True, percentage=50)
+session.set_comments(["Nice!"])
+session.set_quota_supervisor(enabled=True, peak_comments_daily=240, peak_comments_hourly=21)
+
+
+session.end()
